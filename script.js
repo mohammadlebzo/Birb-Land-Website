588ba7238f3c2bc764d2adb41de9f25773442cb9
mohammadlebzo <>
2021-07-14 13:43:14 +0000

    made the website

---
+++ script.js
@@ -0,0 +1,29 @@
+function takeInput()
+{
+  
+  let name = prompt("What is your name ?");
+  console.log(name);
+
+  let favBird = prompt("which is your favorite bird ? \n Cockatiel \n Cockatoo \n Casco");
+  console.log(favBird);
+  
+  let massage = name + "'s favorite bird is:" + favBird;
+
+  if(favBird == "Cockatiel" || favBird == "cockatiel")
+  {
+    document.getElementById("fav").innerHTML = massage;   
+  }
+  else if(favBird == "Cockatoo" || favBird == "cockatoo")
+  {
+    document.getElementById("fav").innerHTML = massage;
+  }
+  else if(favBird == "Casco" || favBird == "casco")
+  {
+    document.getElementById("fav").innerHTML = massage;
+  }
+  else
+  {
+    console.log("not supported");
+  }
+}
+
